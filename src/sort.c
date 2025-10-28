b492ed970bd467a356bc8344d973a49ba016788e
cnn_36@yahoo.dk <cnn_36@yahoo.dk>
2025-10-28 21:20:31 +0100

    1.1

--- src/sort.c
+++ src/sort.c
@@ -2,36 +2,35 @@
 #include <ctype.h>
 #include <stdio.h>
 
-bool pair_is_in_order(char a, char b)
+bool pair_is_in_order(char a, char b) //Checks if array index pair is in order
 {        
-    if(isalpha(a) && isalpha(b))
-    {
+    if(isalpha(a) && isalpha(b)){ //this checks if a and b are letters, if yes,  we check if that lowercase a <= lowercase b
         char new_a;
         char new_b;
-        new_a = tolower(a);
+        new_a = tolower(a); //lowercase on a and b to eliminate case-sensitivity
         new_b = tolower(b);  
         if(new_a <= new_b)        
-            return true;
+            return true; // if lowercase a <= lowercase b, return true
         else
-            return false;  
+            return false; 
     }   
-    return a <= b;
+    return a <= b; // a and b are numbers, return true if a <= b
 }
 
-void swap(char arr[], int i, int j)
+void swap(char arr[], int i, int j) //function to swap the pairs if called on by bubble_sort
 {        
-        char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;    
+        char temp = arr[i]; //saves index i to temp
+        arr[i] = arr[j]; //saves next index j to index i
+        arr[j] = temp; // now saves temp (i) back to the j index spot
 }
 
-void bubble_sort(char arr[], int arr_length, bool (*pair_is_in_order)(char a, char b))
+void bubble_sort(char arr[], int arr_length, bool (*pair_is_in_order)(char a, char b)) //Function that does the sorting
 {
-    for (size_t i = 0; i < arr_length - 1; i++)
+    for (size_t i = 0; i < arr_length - 1; i++) //Iterates through the length of the array -1
     {
-        for (size_t j = 0; j < arr_length -1; j++)
+        for (size_t j = 0; j < arr_length -1; j++) //Looking at pairs in array; [j] and [j+1]
         {
-            if (pair_is_in_order(arr[j],arr[j + 1]) == false)
+            if (pair_is_in_order(arr[j],arr[j + 1]) == false) //If pair is not yet ordered, run swap()
             {
                 swap(arr, j, j+1);
             }
@@ -40,15 +39,15 @@ void bubble_sort(char arr[], int arr_length, bool (*pair_is_in_order)(char a, ch
 }
 
 
-void print_array(char arr[], int arr_length)
+void print_array(char arr[], int arr_length) //Function to print char array
 {
-    for (size_t i = 0; i < arr_length; i++)
+    for (size_t i = 0; i < arr_length; i++) 
     {
         printf("%c ", arr[i]);
     }
     printf("\n");
 }
-void print_array_number(char arr[], int arr_length)
+void print_array_number(char arr[], int arr_length) //Function to print number array
 {
     for (size_t i = 0; i < arr_length; i++)
     {
